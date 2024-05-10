499befd4494069c1559282ae997e2adab8458e3c
Kaustubh <kaustubhrakhade@gmail.com>
2024-05-10 20:09:37 +0530

    fix responsive

--- app.js
+++ app.js
@@ -206,3 +206,17 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach(el => { observer.observe(el); })
 
+
+
+let chatVisible = false;
+const chatWindow = document.querySelector("#chatWindow")
+
+function toggleChat() {
+    chatVisible = !chatVisible;
+    if (chatVisible) {
+        chatWindow.classList.add('show');
+    }
+    else {
+        chatWindow.classList.remove('show');
+    }
+}
