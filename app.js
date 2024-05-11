bcbf478b5d86d1ec61a458048ba2670f3c41f83a
Kaustubh <kaustubhrakhade@gmail.com>
2024-05-11 08:49:58 +0530

    added transition

--- app.js
+++ app.js
@@ -1,4 +1,48 @@
 
+// Scroll between the 4 frames
+
+const AUTO_DELAY = 3600; // time b/w auto slide change
+let currentFrame = 0;
+
+let frames = ["WORKFLOWS", "MESSAGING", "ORDERS", "PAYMENTS"]
+
+let scrollActive = true;
+let pageScrollable = false;
+
+const elements = document.querySelectorAll('#hero-images img');
+const heroControl = document.querySelector('#hero-headers-controls');
+const trustedByControl = document.querySelector('#trustedBy');
+
+function goToFrame(i) {
+    heroControl.style.marginTop = `${-i * 6}rem`;
+    elements.forEach((el, j) => {
+        if (j === i) {
+            el.classList.remove('hidden')
+            el.classList.add('visible')
+        }
+        else {
+            el.classList.remove('visible')
+            el.classList.add('hidden')
+        }
+    })
+
+    if (i === elements.length - 1) {
+        setTimeout(() => { pageScrollable = true; }, 500);
+    } else {
+        pageScrollable = false;
+    }
+}
+
+//infinite gotoframe
+
+function nextFrame() {
+    currentFrame = (currentFrame + 1) % elements.length;
+    goToFrame(currentFrame);
+    setTimeout(() => { nextFrame(); }, AUTO_DELAY);
+}
+
+setTimeout(() => { nextFrame(); }, AUTO_DELAY);
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
