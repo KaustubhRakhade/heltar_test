bba9034bb19bfd50446c6676fc4389cb60b9f0aa
Kaustubh <kaustubhrakhade@gmail.com>
2024-05-12 20:25:46 +0530

    solutions page added

--- app.js
+++ app.js
@@ -43,19 +43,6 @@ function nextFrame() {
 
 setTimeout(() => { nextFrame(); }, AUTO_DELAY);
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-    })
-}, { threshold: 0.5 });
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach(el => { observer.observe(el); })
-
-
-
 let chatVisible = false;
 const chatWindow = document.querySelector("#chatWindow")
 
