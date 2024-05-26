fa72595668fe053032d50249257e95f03476eedd
Kaustubh <kaustubhrakhade@gmail.com>
2024-05-26 12:02:23 +0530

    footer

--- scripts/scrollpop.js
+++ scripts/scrollpop.js
@@ -8,3 +8,26 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach(el => { observer.observe(el); })
+
+let blacksections = document.querySelectorAll('.blackBG');
+
+// if a blacksection is in top 80px of the screen, make the navbar black
+function checkBlackSections() {
+    let isBlack = false;
+    blacksections.forEach((section) => {
+        let rect = section.getBoundingClientRect();
+        if (rect.top < 80 && rect.bottom > 0) {
+            isBlack = true;
+        }
+    })
+
+    if (isBlack) {
+        document.querySelector('#navbar-container').classList.add('black');
+    } else {
+        document.querySelector('#navbar-container').classList.remove('black');
+    }
+}
+
+document.addEventListener('scroll', () => {
+    checkBlackSections();
+})
