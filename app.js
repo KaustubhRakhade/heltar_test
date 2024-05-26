edbd33763dd955735f7722c1ca30d997df5bcf0d
Kaustubh <kaustubhrakhade@gmail.com>
2024-05-26 15:08:21 +0530

    form

--- app.js
+++ app.js
@@ -71,50 +71,85 @@ function nextFrame() {
 
 setTimeout(() => { nextFrame(); }, AUTO_DELAY);
 
-// scroll between reviews
+let promiseInterval;
+let currP = 0;
+function setPromise(n) {
 
-// let currentReview = 1; 
+    // claer the interval
 
-// for (let i = 1; i <= 6; i++) {
-//     document.querySelector(`#r${i}`).addEventListener('click', () => {
-//         goToReview(i);
-//     })
-// }
+    clearInterval(promiseInterval);
 
-// let reviewInterval = setInterval(() => {
-//     if (currentReview < 6) {
-//         goToReview(currentReview + 1);
-//     } else {
-//         goToReview(1);
-//     }
-// }, 5000);
+    promiseInterval = setInterval(() => {
 
-// function goToReview(i) {
-//     document.querySelector('#reviewList').scrollTo({ left: (i - 1) * window.innerWidth, behavior: 'smooth' });
-//     for (let j = 1; j <= 6; j++) {
-//         if (j === i) { document.querySelector(`#r${j}`).classList.add('active'); }
-//         else { document.querySelector(`#r${j}`).classList.remove('active'); }
-//     }
-// }
+        setPromise((currP + 1) % 3);
 
-// function getCurrentReview() {
-//     currentReview = Math.round(document.querySelector('#reviewList').scrollLeft / window.innerWidth) + 1;
-//     for (let j = 1; j <= 6; j++) {
-//         if (j === currentReview) { document.querySelector(`#r${j}`).classList.add('active'); }
-//         else { document.querySelector(`#r${j}`).classList.remove('active'); }
-//     }
-// }
-// document.querySelector('#reviewList').addEventListener('scroll', () => {
-//     clearInterval(reviewInterval);
-//     reviewInterval = setInterval(() => {
-//         if (currentReview < 6) {
-//             goToReview(currentReview + 1);
-//         } else {
-//             goToReview(1);
-//         }
-//     }, 5000);
-//     getCurrentReview();
-// })
+    }, 5000);
+
+
+    let ps = document.querySelectorAll('.promiseSliderItem');
+    let pg = document.querySelectorAll('#pageControls div');
+
+    if (currP === n) return;
+
+
+    let dir = -1;
+    if ((n - currP + 3) % 3 == 1) dir = 1;
+
+    currP = n;
+
+    for (let i = 0; i < 3; i++) {
+
+        if (i === n) {
+            pg[i].classList.add('active');
+        }
+        else {
+            pg[i].classList.remove('active');
+        }
+    }
+
+
+    card1 = document.querySelectorAll('.card1')[0];
+    card2 = document.querySelectorAll('.card2')[0];
+    card3 = document.querySelectorAll('.card3')[0];
+
+
+    if (dir === -1) {
+        card1.style.animation = 'card12 0.5s forwards';
+        card1.classList.remove("card1");
+        card1.classList.add("card2");
+
+        card2.style.animation = 'card23 0.5s forwards';
+        card2.classList.remove("card2");
+        card2.classList.add("card3");
+
+        card3.style.animation = 'card31 0.5s forwards';
+        card3.classList.remove("card3");
+        card3.classList.add("card1");
+
+    } else {
+
+        card1.style.animation = 'card13 0.5s forwards';
+        card1.classList.remove("card1");
+        card1.classList.add("card3");
+
+        card2.style.animation = 'card21 0.5s forwards';
+        card2.classList.remove("card2");
+        card2.classList.add("card1");
+
+        card3.style.animation = 'card32 0.5s forwards';
+        card3.classList.remove("card3");
+        card3.classList.add("card2");
+
+    }
+
+}
+
+
+
+
+promiseInterval = setInterval(() => {
+    setPromise((currP + 1) % 3);
+}, 5000);
 
 
 let currF = 1;
